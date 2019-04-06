1682199bb0c1dd4eb545e5c4df1e074b7d0dc6cf
Alan Casas <alancasasarevalo@gmail.com>
2019-04-06 12:02:36 +0200

    [FEATURE] [FORM] - Custom error was implemented

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Header from "./Header";
 import Form from "./Form";
+import CustomError from "./CustomError";
 
 class App extends Component {
     state = {
         error: ''
-    }
+    };
 
     componentDidMount() {
         this.setState({
@@ -15,6 +16,9 @@ class App extends Component {
 
     weatherSearched = response => {
         if (response) {
+            this.setState({
+                error: false
+            })
         } else {
             this.setState({
                 error: true
@@ -23,6 +27,17 @@ class App extends Component {
     };
 
     render() {
+
+        const error = this.state.error;
+        let result = '';
+        if (error) {
+            result = <CustomError
+                message={'Todos los campos son obligatorios'}
+            />
+        } else {
+
+        }
+
         return (
             <div className="App">
                 <Header
@@ -31,6 +46,7 @@ class App extends Component {
                 <Form
                     weatherSearched={this.weatherSearched}
                 />
+                {result}
             </div>
         );
     }
