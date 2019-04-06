f436bb418abbb003908ea6e0c7fefaa15eed9ef4
Alan Casas <alancasasarevalo@gmail.com>
2019-04-06 11:53:14 +0200

    [FEATURE][FORM] - Form was implemented

--- src/component/App.js
+++ src/component/App.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from "./Header";
 import Form from "./Form";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Header
-            title="Weather"
-          />
-          <Form
-          />
-      </div>
-    );
-  }
+    state = {
+        error: ''
+    }
+
+    componentDidMount() {
+        this.setState({
+            error: false
+        })
+    }
+
+    weatherSearched = response => {
+        if (response) {
+        } else {
+            this.setState({
+                error: true
+            })
+        }
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Header
+                    title="Weather"
+                />
+                <Form
+                    weatherSearched={this.weatherSearched}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/component/Form.js
+++ src/component/Form.js
@@ -1,18 +1,48 @@
 import React, {Component} from 'react'
 
 class Form extends Component{
+
+    // crear refs
+    cityRef = React.createRef();
+    countryRef = React.createRef();
+
+    searchWeather = (e) => {
+        e.preventDefault();
+
+    //    leer refs
+        const citySearched = this.cityRef.current.value;
+        const countrySearched = this.countryRef.current.value;
+
+        if (citySearched && typeof citySearched !== 'undefined' && countrySearched && typeof countrySearched !== 'undefined'){
+        //    crear el objeto
+            const weatherResponse = {
+                citySearched,
+                countrySearched
+            }
+        //    enviarlo a app
+            this.props.weatherSearched(weatherResponse)
+
+        } else {
+            this.props.weatherSearched(null)
+        }
+
+
+    //    reset form
+        e.currentTarget.reset();
+    };
+
     render() {
         return (
             <div className="contenedor-form">
                 <div className="container">
                     <div className="row">
-                        <form>
+                        <form onSubmit={this.searchWeather}>
                             <div className="input-field col s12 m8 l4 offset-m2">
-                                <input id="ciudad" type="text"/>
+                                <input id="ciudad" type="text" ref={this.cityRef}/>
                                 <label htmlFor="ciudad"> Ciudad: </label>
                             </div>
                             <div className="input-field col s12 m8 l4 offset-m2">
-                                <select>
+                                <select ref={this.countryRef}>
                                     <option value="" defaultValue> Elige un pais </option>
                                     <option value="AR">Argentina</option>
                                     <option value="ES">España</option>
